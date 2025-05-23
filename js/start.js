35db7b3e054ab50e342fa6e52890142d67437e4c
GabrielKobielski <kobielski2002@gmail.com>
2025-05-22 22:47:07 -0300

    projetopronto

--- js/start.js
+++ js/start.js
@@ -2,6 +2,7 @@ let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
 
 trilho.addEventListener('click',()=>{
+  setTimeout(segu)
   trilho.classList.toggle('light')
   body.classList.toggle('light')
 })
