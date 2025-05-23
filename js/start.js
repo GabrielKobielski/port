836488ea4a22dad3f4b49ae95033fe7f18ca5230
GabrielKobielski <kobielski2002@gmail.com>
2025-05-23 16:46:35 -0300

    alteracao-stara

--- js/start.js
+++ js/start.js
@@ -2,7 +2,6 @@ let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
 
 trilho.addEventListener('click',()=>{
-  setTimeout(segu)
   trilho.classList.toggle('light')
   body.classList.toggle('light')
 })
